cda99110899c5f480bdcf0fbf516c510d9248379
Alua Baikadamova <abaikad00@gmail.com>
2019-01-16 04:44:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_accessor :name1, :name2, :name3
+  
+  def initialize(name1, name2, name3)
+  @name1 = name1
+  @name2 = name2
+  @name3 = name3
+  
+  end
+  
+  def kind (lalala=self)
+   self.kind
+    
+  #  if (@name1>=@name2+@name3)||(@name2>=@name1+@name3)||(@name3>=@name2+@name1)||(@name1<=0)||(@name2<=0)||(@name3<=0)
+   #  begin
+       raise TriangleError
+     rescue TriangleError => error
+         puts error.message
+  #   end
+ #   else
+ #     return :equilateral
+ #  end
+  end
+  
+  class TriangleError < StandardError
+    def message 
+      "lalalla"
+    end
+  end
 end
