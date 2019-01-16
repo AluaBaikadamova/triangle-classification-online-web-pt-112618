ca3df5affa674b063395dbc046c51bcbbdc147bf
Alua Baikadamova <abaikad00@gmail.com>
2019-01-16 04:49:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,8 +14,8 @@ class Triangle
   #  if (@name1>=@name2+@name3)||(@name2>=@name1+@name3)||(@name3>=@name2+@name1)||(@name1<=0)||(@name2<=0)||(@name3<=0)
    #  begin
        raise TriangleError
-     rescue TriangleError => error
-         puts error.message
+   #  rescue TriangleError => error
+   #      puts error.message
   #   end
  #   else
  #     return :equilateral
